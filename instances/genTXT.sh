50c77d804f9e0613c442c817321f00d227fb2b8a
Jordi Coll <jordicoll@udg.edu>
2024-02-29 16:39:24 +0100

    Updated instances from the experiments

--- instances/genTXT.sh
+++ instances/genTXT.sh
@@ -1,9 +1,9 @@
-for i in `seq 1 20`; do 
-	for A in 10 15 20 25 30; do 
+for i in `seq 1 50`; do 
+	for A in 10 15 20 25 30 35; do 
 		for V in 4 5; do  
-			U=30;  
+			U=40;  
 			echo instance_${U}_${A}_${V}_${i}.txt
-			python3 create_instances.py $U $A $V > TXT_10-30_4-5_NEW/instance_${U}_${A}_${V}_${i}.txt 2>/dev/null
+			python3 create_instances.py $U $A $V > TXT_10-35_4-5_U40/instance_${U}_${A}_${V}_${i}.txt 2>/dev/null
 		done; 
 	done; 
 done
--- instances/genWCNF.sh
+++ instances/genWCNF.sh
@@ -1,7 +1,7 @@
-for i in TXT_10-30_4-5_NEW/*; do 
+for i in TXT_10-35_4-5_U40/*; do 
 	echo $i;
 	name=$(basename $i); 
-	../bin/release/rl2sat -e=1 -f=dimacs -S=0 $i > WCNF_10-30_4-5_NEW/${name}_HARD.wcnf ; 
-	../bin/release/rl2sat -e=1 -f=dimacs -S=1 $i > WCNF_10-30_4-5_NEW/${name}_SOFT.wcnf ;
+	../bin/release/rl2sat -e=1 -f=dimacs -S=0 $i > WCNF_10-35_4-5_U40/${name}_HARD.wcnf ; 
+	../bin/release/rl2sat -e=1 -f=dimacs -S=1 $i > WCNF_10-35_4-5_U40/${name}_SOFT.wcnf ;
 done
 
